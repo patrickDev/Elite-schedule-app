3947b54a9dd3a09bba0eec9978634cd339141628
Patrick <patrick_dev@outlook.com>
2018-10-30 23:21:47 -0500

    Navigation Parameters

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -2,26 +2,24 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { TeamDetailsPage } from '../team-details/team-details';
 
-/**
- * Generated class for the TeamsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-teams',
   templateUrl: 'teams.html',
 })
 export class TeamsPage {
-
+ public teams = [
+   {id:1, name: 'HC Elite'},
+   {id:2, name: 'Team Takeover'},
+   {id:3, name: 'DC Thunder'}
+ ]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TeamsPage');
   }
-  itemTapped(){
-    this.navCtrl.push(TeamDetailsPage)
+  itemTapped($event, team){
+    this.navCtrl.push(TeamDetailsPage, team)
   }
 }
